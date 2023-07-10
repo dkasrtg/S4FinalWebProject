57771ae7e691376c4088ff5c5ac25ae428f2d1a4
ChristelRav <ravmihary@gmail.com>
2023-07-10 09:39:47 +0300

    Page_admin_intro

---
+++ application/views/admin/template/footer.php
@@ -0,0 +1,38 @@
+<!--container-scroller-->
+<!--plugins: js-->
+<script src="<?= bu("assets_admin/vendors/js/vendor.bundle.base.js") ?>"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src="<?= bu("assets_admin/js/off-canvas.js") ?>"></script>
+<script src="<?= bu("assets_admin/js/hoverable-collapse.js") ?>"></script>
+<script src="<?= bu("assets_admin/js/misc.js") ?>"></script>
+<script src="<?= bu("assets_admin/js/settings.js") ?>"></script>
+<script src="<?= bu("assets_admin/js/todolist.js") ?>"></script>
+<script src="<?= bu("assets_admin/js/jquery/jquery.js") ?>"></script>
+<!--endinject-->
+<!--Custom js for this page -->
+<!-- End custom js for this page-->
+<!-- personal script -->
+<script>
+    // function for sending a formData using jquery
+    function sendFormData(form, url, method, callback) {
+        var formData = new FormData(form);
+        $.ajax({
+            url: url,
+            type: method,
+            data: formData,
+            success: function(data) {
+                callback(data);
+            },
+            cache: false,
+            contentType: false,
+            processData: false
+        });
+    }
+</script>
+</body>
+
+
+</html>
