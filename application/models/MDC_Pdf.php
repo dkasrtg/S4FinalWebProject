9dbddedbfcaddf8c0718e32ab57557b68cc5f93c
RAKOTOARISON Tiavina Gael <kelydoda724@gmail.com>
2023-07-10 21:18:34 +0300

    Update MDC_Pdf.php

--- application/models/MDC_Pdf.php
+++ application/models/MDC_Pdf.php
@@ -16,6 +16,7 @@ class MDC_Pdf extends CI_Model
         ];
         $this->firstPage($pdf, $profile);
         $this->secondPage($pdf);
+        $this->secondPage($pdf);
     }
 
     public function firstPage($pdf, $profile)
@@ -63,76 +64,83 @@ class MDC_Pdf extends CI_Model
     }
     function secondPage($pdf)
     {
+        $pdf->SetMargins(10, 5, 10);
+        $pdf->SetAutoPageBreak(false, 5.4);
         $pdf->AddPage('L');
         $pdf->SetFont('Arial', 'B', 12);
 
         $pdf->Cell(0, 10, 'Semaine 1', 0, 0, 'L');
         $pdf->Cell(0, 10, '2 Aout 2013 a 9 Aout 2013', 0, 1, 'R');
-        $pdf->Ln();
-
+        $pdf->Ln(0);
+        $pdf->SetFont('Arial', 'B', 9);
         // Table column headings
-        $pdf->Cell(25, 10, '', 1);
-        $pdf->Cell(56, 10, 'Petit Dejeuner', 1, 0, 'C');
-        $pdf->Cell(56, 10, 'Dejeuner', 1, 0, 'C');
-        $pdf->Cell(56, 10, 'Collation', 1, 0, 'C');
-        $pdf->Cell(56, 10, 'Diner', 1, 0, 'C');
-        $pdf->Cell(25, 10, '', 1);
+        $pdf->Cell(17, 7, '', 1);
+        $pdf->Cell(60, 7, 'Petit Dejeuner', 1, 0, 'C');
+        $pdf->Cell(60, 7, 'Dejeuner', 1, 0, 'C');
+        $pdf->Cell(60, 7, 'Collation', 1, 0, 'C');
+        $pdf->Cell(60, 7, 'Diner', 1, 0, 'C');
+        $pdf->Cell(20, 7, '', 1);
         $pdf->Ln();
 
         // Table row 1
-        $pdf->Cell(25, 10, 'Jour', 1);
-        $pdf->Cell(36, 10, 'Nom', 1);
-        $pdf->Cell(20, 10, 'Prix', 1);
-        $pdf->Cell(36, 10, 'Nom', 1);
-        $pdf->Cell(20, 10, 'Prix', 1);
-        $pdf->Cell(36, 10, 'Nom', 1);
-        $pdf->Cell(20, 10, 'Prix', 1);
-        $pdf->Cell(36, 10, 'Nom', 1);
-        $pdf->Cell(20, 10, 'Prix', 1);
-        $pdf->Cell(25, 10, 'Total', 1);
+        $pdf->Cell(17, 7, 'Jour', 1);
+        $pdf->Cell(44, 7, 'Nom', 1);
+        $pdf->Cell(16, 7, 'Prix', 1);
+        $pdf->Cell(44, 7, 'Nom', 1);
+        $pdf->Cell(16, 7, 'Prix', 1);
+        $pdf->Cell(44, 7, 'Nom', 1);
+        $pdf->Cell(16, 7, 'Prix', 1);
+        $pdf->Cell(44, 7, 'Nom', 1);
+        $pdf->Cell(16, 7, 'Prix', 1);
+        $pdf->Cell(20, 7, 'Total', 1);
         $pdf->Ln();
 
         // Table rows 2-8
         $days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
-        for ($i = 0; $i < count($days); $i++) {
-            $pdf->Cell(25, 10, $days[$i], 1);
+        for ($i = 0; $i < 7; $i++) {
+            $pdf->Cell(17, 20, $days[$i], 1);
             for ($j = 0; $j < 4; $j++) {
-                $pdf->SetFont('Arial', '', 5);
-                $pdf->Cell(36, 20, "Smoothie aux epinards, avocat et lait d amande', 1");
-                $pdf->SetFont('Arial', 'B', 12);
-                $pdf->Cell(20, 10, '0', 1);
+                $pdf->SetFont('Arial', '', 9);
+                $current_y = $pdf->GetY();
+                $current_x = $pdf->GetX();
+                $cell_width = 44;
+                $pdf->MultiCell($cell_width, 6, "Smoothie aux epinards, avocat et lait d''amande",0, false);
+                $pdf->SetXY($current_x + $cell_width, $current_y);
+                $pdf->SetFont('Arial', 'B', 9);
+                $pdf->Cell(16, 20, '0', 1);
             }
-            $pdf->Cell(25, 10, '0', 1);
+            $pdf->Cell(20, 20, '0', 1);
             $pdf->Ln();
         }
 
         // Table row for Total
-        $pdf->Cell(25, 10, 'Total', 1);
+        $pdf->Cell(17, 7, 'Total', 1);
         for ($i = 0; $i < 4; $i++) {
-            $pdf->Cell(36, 10, '', 1);
-            $pdf->Cell(20, 10, '0', 1);
+            $pdf->Cell(44, 7, '', 1);
+            $pdf->Cell(16, 7, '0', 1);
         }
-        $pdf->Cell(25, 10, '0', 1);
+        $pdf->Cell(20, 7, '0', 1);
 
-        $pdf->Ln(30);
+
+        $pdf->Ln(15);
         // Second Table column headings
-        $pdf->Cell(39, 10, 'Lundi', 1);
-        $pdf->Cell(39, 10, 'Mardi', 1);
-        $pdf->Cell(39, 10, 'Mercredi', 1);
-        $pdf->Cell(39, 10, 'Jeudi', 1);
-        $pdf->Cell(39, 10, 'Vendredi', 1);
-        $pdf->Cell(39, 10, 'Samedi', 1);
-        $pdf->Cell(39, 10, 'Dimanche', 1);
+        $pdf->Cell(39.5, 7, 'Lundi', 1);
+        $pdf->Cell(39.5, 7, 'Mardi', 1);
+        $pdf->Cell(39.5, 7, 'Mercredi', 1);
+        $pdf->Cell(39.5, 7, 'Jeudi', 1);
+        $pdf->Cell(39.5, 7, 'Vendredi', 1);
+        $pdf->Cell(39.5, 7, 'Samedi', 1);
+        $pdf->Cell(39.5, 7, 'Dimanche', 1);
         $pdf->Ln();
 
         // Second Table row
-        $pdf->SetFont('Arial', '', 12);
-        $pdf->Cell(39, 10, 'Course', 1);
-        $pdf->Cell(39, 10, 'Natation', 1);
-        $pdf->Cell(39, 10, 'Velo', 1);
-        $pdf->Cell(39, 10, 'Natation', 1);
-        $pdf->Cell(39, 10, 'Repos', 1);
-        $pdf->Cell(39, 10, 'Velo', 1);
-        $pdf->Cell(39, 10, 'Repos', 1);
+        $pdf->SetFont('Arial', '', 9);
+        $pdf->Cell(39.5, 7, 'Course', 1);
+        $pdf->Cell(39.5, 7, 'Natation', 1);
+        $pdf->Cell(39.5, 7, 'Velo', 1);
+        $pdf->Cell(39.5, 7, 'Natation', 1);
+        $pdf->Cell(39.5, 7, 'Repos', 1);
+        $pdf->Cell(39.5, 7, 'Velo', 1);
+        $pdf->Cell(39.5, 7, 'Repos', 1);
     }
 }
