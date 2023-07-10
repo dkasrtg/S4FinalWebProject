146095be4eb7f7141ba4b00b8c4201a0263ce4ea
ChristelRav <ravmihary@gmail.com>
2023-07-10 15:29:09 +0300

    repas_mdodel

---
+++ application/models/MDA_Repas.php
@@ -0,0 +1,56 @@
+
+<?php
+
+class MDA_Repas extends CI_Model 
+{
+
+    public function __construct() 
+    {
+        parent::__construct();
+    }
+
+    // CREATE
+    public function insert_repas($data)
+    {
+        $this->db->insert('repas', $data);
+        $insert_id = $this->db->insert_id();
+        return $this->get_repas($insert_id);
+    }
+
+   //SELECT CATEGORIE
+   public function get_categorie_repas(){
+        $query = $this->db->get('categorie_repas');
+        return $query->result();
+   } 
+
+    // READ
+    public function get_repas($id_repas = NULL){
+        if ($id_repas !== NULL) {
+            $query = $this->db->get_where('repas', array('id_repas' => $id_repas));
+            return $query->row_array();
+        } else {
+            $this->db->select('repas.*, categorie_repas.nom_categorie');
+            $this->db->from('repas');
+            $this->db->join('categorie_repas', 'repas.id_categorie_repas = categorie_repas.id_categorie_repas');
+            $query = $this->db->get();
+            return $query->result();
+        }
+    }
+
+    // UPDATE
+    public function update_repas($id_repas, $data) 
+    {
+        $this->db->where('id_repas', $id_repas);
+        $this->db->update('repas', $data);
+        return $this->db->affected_rows();
+    }
+
+    // DELETE
+    public function delete_repas($id_repas) 
+    {
+        $this->db->where('id_repas', $id_repas);
+        $this->db->delete('repas');
+        return $this->db->affected_rows();
+    }
+    
+}
