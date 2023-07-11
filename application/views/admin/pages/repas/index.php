25edd5dffa718f1a81400518c830f4f6fc3f3857
ChristelRav <ravmihary@gmail.com>
2023-07-11 06:41:55 +0300

    Map_js_css_Menu

---
+++ application/views/admin/pages/repas/index.php
@@ -0,0 +1,206 @@
+
+        <div class="main-panel">
+          <div class="content-wrapper">
+            <div class="page-header">
+              <h3 class="page-title"> Chart-js </h3>
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="#">Accueil</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">Socicete FAKO'MA</li>
+                </ol>
+              </nav>
+            </div>
+             <!--body of content -->
+            <div class="row">
+               <!-- courbe de Vente -->
+              <div class="col-lg-6 grid-margin stretch-card">
+                <div class="card" >
+                  <div class="card-body">
+                    <h4 class="card-title">chart d'évolution</h4>
+                    <canvas id="doughnutChart" style="height:250px;"></canvas>
+                  </div>
+                </div>
+              </div>
+               <!-- Societe et action -->
+               <div class="col-6 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="card-title">Bilan de la Société</h4>
+                        <div class="table-responsive">
+                          <table class="table">
+                            <tbody>
+                              <tr>
+                                <td>
+                                   <i class="mdi mdi-shopping"></i>
+                                </td>
+                                <td>Produits mise en vente</td>
+                                <td class="text-right"> 1500 </td>
+                                <td class="text-right font-weight-medium"> 56.35% </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <i class="mdi mdi-slack"></i>
+                                </td>
+                                <td>Chiffre d'affaire</td>
+                                <td class="text-right"> 800 </td>
+                                <td class="text-right font-weight-medium"> 33.25% </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                   <i class="mdi mdi-account-star"></i>
+                                </td>
+                                <td>Meilleur Client</td>
+                                <td class="text-right"> 760 </td>
+                                <td class="text-right font-weight-medium"> 15.45% </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <i class="mdi mdi-stocking"></i>
+                                </td>
+                                <td>Stock de Charbon</td>
+                                <td class="text-right"> 450 </td>
+                                <td class="text-right font-weight-medium"> 25.00% </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <i class="mdi mdi-stocking"></i>
+                                </td>
+                                <td>Stock de composte</td>
+                                <td class="text-right"> 620 </td>
+                                <td class="text-right font-weight-medium"> 10.25% </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <i class="mdi mdi-stocking"></i>
+                                </td>
+                                <td>Stock de ciment</td>
+                                <td class="text-right"> 230 </td>
+                                <td class="text-right font-weight-medium"> 75.00% </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                   
+                </div>
+              </div>
+               <!-- Other info -->
+          </div>
+          <div class="row">
+              <div class="col-sm-4 grid-margin">
+                <div class="card">
+                  <div class="card-body">
+                    <h5>Revenue</h5>
+                    <div class="row">
+                      <div class="col-8 col-sm-12 col-xl-8 my-auto">
+                        <div class="d-flex d-sm-block d-md-flex align-items-center">
+                          <h2 class="mb-0">$32123</h2>
+                          <p class="text-success ml-2 mb-0 font-weight-medium">+3.5%</p>
+                        </div>
+                        <h6 class="text-muted font-weight-normal">11.38% Since last month</h6>
+                      </div>
+                      <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
+                        <i class="icon-lg mdi mdi-codepen text-primary ml-auto"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-4 grid-margin">
+                <div class="card">
+                  <div class="card-body">
+                    <h5>Vendues</h5>
+                    <div class="row">
+                      <div class="col-8 col-sm-12 col-xl-8 my-auto">
+                        <div class="d-flex d-sm-block d-md-flex align-items-center">
+                          <h2 class="mb-0">$45850</h2>
+                          <p class="text-success ml-2 mb-0 font-weight-medium">+8.3%</p>
+                        </div>
+                        <h6 class="text-muted font-weight-normal"> 9.61% Since last month</h6>
+                      </div>
+                      <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
+                        <i class="icon-lg mdi mdi-wallet-travel text-danger ml-auto"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-4 grid-margin">
+                <div class="card">
+                  <div class="card-body">
+                    <h5>Purchase</h5>
+                    <div class="row">
+                      <div class="col-8 col-sm-12 col-xl-8 my-auto">
+                        <div class="d-flex d-sm-block d-md-flex align-items-center">
+                          <h2 class="mb-0">$2039</h2>
+                          <p class="text-danger ml-2 mb-0 font-weight-medium">-2.1% </p>
+                        </div>
+                        <h6 class="text-muted font-weight-normal">2.27% Since last month</h6>
+                      </div>
+                      <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
+                        <i class="icon-lg mdi mdi-monitor text-success ml-auto"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Info societe -->
+            <div class="row">
+            <div class="col-lg-12 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="card-title">Infomation fako'ma</h4>
+                    <p class="card-description"> Perso <code>.Société</code>
+                    </p>
+                    <div class="table-responsive">
+                    <table class="table">
+                        <tbody>
+                        <tr>
+                          <th>Nom</th>
+                          <td>FAKO'MA</td>
+                        </tr>
+                        <tr>
+                            <th>Logo</th>
+                            <td>FAKO'MA</td>
+                        </tr>
+                        <tr>
+                          <th>Siege</th>
+                          <td>Toamasina</td>
+                        </tr>
+                        <tr>
+                          <th>Adresse d'exploitation</th>
+                          <td>Lot 0205 AK Anatanarivo</td>
+                        </tr>
+                        <tr>
+                          <th>Date de creation</th>
+                          <td>Année 2023</td>
+                        </tr>
+                        <tr>
+                          <th>Numero d'identification fiscale</th>
+                          <td>FISC002548</td>
+                        </tr>
+                        <tr>
+                          <th>Numero statistique</th>
+                          <td>0011223344</td>
+                        </tr>
+                        <tr>
+                          <th>Numero de registre de commerce</th>
+                          <td>REF123456789</td>
+                        </tr>
+                        <tr>
+                          <th>Contact</th>
+                          <td>Mail : Fakoma@gmail.com</td>
+                        </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+    </div>
+  
+  </body>
+</html>
--- application/views/admin/template/footer.php
+++ application/views/admin/template/footer.php
@@ -1,41 +1,18 @@
-<!--container-scroller-->
-<!--plugins: js-->
-<script src="<?= base_url("assets_admin/vendors/js/vendor.bundle.base.js") ?>"></script>
-<!-- endinject -->
-<!-- Plugin js for this page -->
-<!-- End plugin js for this page -->
-<!-- inject:js -->
-<script src="<?= base_url("assets_admin/js/off-canvas.js") ?>"></script>
-<script src="<?= base_url("assets_admin/js/hoverable-collapse.js") ?>"></script>
-<script src="<?= base_url("assets_admin/js/misc.js") ?>"></script>
-<script src="<?= base_url("assets_admin/js/settings.js") ?>"></script>
-<script src="<?= base_url("assets_admin/js/todolist.js") ?>"></script>
-<script src="<?= base_url("assets_admin/js/jquery/jquery.js") ?>"></script>
-<script src="<?= base_url("assets_admin/js/chart.js") ?>"></script>
-<script src="<?= base_url("assets_admin/vendors/js/vendor.bundle.base.js") ?>"></script>
-<script src="<?= base_url("assets_admin/vendors/chart.js/Chart.min.js") ?>"></script>
-<!--endinject-->
-<!--Custom js for this page -->
-<!-- End custom js for this page-->
-<!-- personal script -->
-<script>
-    // function for sending a formData using jquery
-    function sendFormData(form, url, method, callback) {
-        var formData = new FormData(form);
-        $.ajax({
-            url: url,
-            type: method,
-            data: formData,
-            success: function(data) {
-                callback(data);
-            },
-            cache: false,
-            contentType: false,
-            processData: false
-        });
-    }
-</script>
-</body>
 
-
-</html>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="<?= base_url("assets_admin/vendors/js/vendor.bundle.base.js") ?>"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="<?= base_url("assets_admin/vendors/chart.js/Chart.min.js") ?>"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="<?= base_url("assets_admin/js/off-canvas.js") ?>"></script>
+    <script src="<?= base_url("assets_admin/js/hoverable-collapse.js") ?>"></script>
+    <script src="<?= base_url("assets_admin/js/misc.js") ?>"></script>
+    <script src="<?= base_url("assets_admin/js/settings.js") ?>"></script>
+    <script src="<?= base_url("assets_admin/js/todolist.js") ?>"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script src="<?= base_url("assets_admin/js/chart.js") ?>"></script>
+    <!-- End custom js for this page -->
