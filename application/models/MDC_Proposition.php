3d1b75cd00618ee25dc60d81fcb38d9a889f0af0
ChristelRav <ravmihary@gmail.com>
2023-07-11 14:34:56 +0300

    IMC_ideal

--- application/models/MDC_Proposition.php
+++ application/models/MDC_Proposition.php
@@ -12,6 +12,25 @@ class MDC_Proposition extends CI_Model
         $this->db->where('min <', $imc);
         $query = $this->db->get('proposition');
         return $query->row_array();
+    } 
+    public function get_imc($poids,$taille){
+        $imc = round($poids/(($taille)^2),2);
+        return $imc;
+    }
+    public function get_poids_ideal($taille) {
+        $poids = 1;
+        $imc = 0;
+        while ($imc < 19 || $imc > 25) {
+            $imc = $poids / ($taille ** 2);
+            if ($imc < 19) {
+                $poids++;
+            } else if ($imc > 25) {
+                $poids--;
+                break;
+            }
+        }
+        return $poids;
     }    
+     
 }
 ?>
